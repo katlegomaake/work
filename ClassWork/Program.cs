d795250eb40dffa168a61c88bab0fdfa685ed8f5
katlegomaake <164087097+katlegomaake@users.noreply.github.com>
2024-04-23 13:19:10 +0200

    Add files via upload

---
+++ ClassWork/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public interface IPlayable
+{
+    void Play();
+}
+
+public class Guitar : IPlayable
+{
+    public void Play()
+    {
+        Console.WriteLine("Guitar is now playing");
+    }
+}
+
+public class Piano : IPlayable
+{
+    public void Play()
+    {
+        Console.WriteLine("Piano is now playing");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Guitar guitar = new Guitar();
+        Piano piano = new Piano();
+
+        
+        guitar.Play();
+
+        
+        piano.Play();
+    }
+}
